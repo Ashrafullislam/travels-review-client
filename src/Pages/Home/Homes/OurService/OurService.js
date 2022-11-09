63a3bfdee6fbad813df8bf357875a6f1143b8892
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-11-09 17:30:09 +0600

    added card of service

--- src/Pages/Home/Homes/OurService/OurService.js
+++ src/Pages/Home/Homes/OurService/OurService.js
@@ -1,16 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard/ServiceCard';
 
 const OurService = () => {
-
+const [services, setServices ] = useState([])
     useEffect(()=> {
-        fetch('')
+        fetch('http://localhost:5000/service')
         .then(res  => res.json())
-        .then(data => console.log(data))
+        .then(data => setServices(data))
     },[])
     return (
-        <div>
-            <h3 className='text-4xl text-center my-6 font-bold'> See my Best Services With Review  </h3>
+        <div className='mx-auto'>
+              <h3 className='text-4xl text-center my-6 font-bold'> See my Best Services With Review {services.length}  </h3>
+            <div className='grid lg:grid-cols-2  gap-6   '>
+          
+            {
+                services.map(service => <ServiceCard key={service._id} service = {service} > </ServiceCard> )
+            }
             
+            </div>
+          
         </div>
     );
 };
